9d715fd61478837dc872334a1c20f17dcc15b726
Tharunn Golthi <txg160430@utdallas.edu>
2018-12-03 14:16:30 -0600

    code refactor

--- src/org/soccer/queryExpansion/stemHelper.java
+++ src/org/soccer/queryExpansion/stemHelper.java
@@ -53,25 +53,7 @@ public class stemHelper {
             }
         }
 
-        List<String> stemsFound;
-        for (String w1: keySet) {
-            flag = false;
-            for(POS pos: values) {
-                stemsFound = this.stemmer.findStems(w1, pos);
-                for (String w: stemsFound){
-                    if (Stems.contains(w)){
-                        flag = true;
-                        this.stemMap.putIfAbsent(w, new HashSet<>());
-                        this.stemMap.get(w).add(w1);
-                    }
-                }
-            }
-
-            if (!flag) {
-                this.stemMap.putIfAbsent(w1, new HashSet<>());
-                this.stemMap.get(w1).add(w1);
-            }
-        }
+        List<String> stemsFound = helper(keySet, values, Stems);
 
         Set<String> tokensSet;
         Stems = new HashSet<>();
@@ -115,5 +97,143 @@ public class stemHelper {
                 this.stemMap.remove(w1);
             }
         }
+    }
+
+    public List<String> helper(Set<String> keySet, POS[] values, Set<String> Stems){
+        List<String> stemsFound = new List<String>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<String> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends String> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, Collection<? extends String> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public String get(int index) {
+                return null;
+            }
+
+            @Override
+            public String set(int index, String element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, String element) {
+
+            }
+
+            @Override
+            public String remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<String> listIterator() {
+                return null;
+            }
+
+            @Override
+            public ListIterator<String> listIterator(int index) {
+                return null;
+            }
+
+            @Override
+            public List<String> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+        for (String w1: keySet) {
+            boolean flag = false;
+            for(POS pos: values) {
+                stemsFound = this.stemmer.findStems(w1, pos);
+                for (String w: stemsFound){
+                    if (Stems.contains(w)){
+                        flag = true;
+                        this.stemMap.putIfAbsent(w, new HashSet<>());
+                        this.stemMap.get(w).add(w1);
+                    }
+                }
+            }
+
+            if (!flag) {
+                this.stemMap.putIfAbsent(w1, new HashSet<>());
+                this.stemMap.get(w1).add(w1);
+            }
+        }
+        return stemsFound;
     }
 }
